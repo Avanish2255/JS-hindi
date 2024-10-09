9e7ae385cdc570eea4f0e8b77fa8d8e1c3c9e25f
Avanish Kumar Mishra <138348943+Avanish2255@users.noreply.github.com>
2024-10-09 12:53:02 +0000

    datatype

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,23 @@
+const accountId=142456;
+let accountEmail= "avanishmishra648@gmail.com"
+var accountPassword = "1234"
+
+accountCity= "Jaunpur"
+accountCity="Noida"
+
+
+let accountState;
+
+// accountId=2 // not allow
+accountEmail="mishraavanish@gmail.com"
+accountPassword="4567"
+
+
+// prefer not use var
+// because of issue in block svpe in functional scope
+
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict";// traet all js cose as newer version
+
+//  alert("hello")// we are using node js. not browser
+
+console.log(3+
+    3); // code readability should be high
+
+
+console.log("Avanish");
+
+
+let name ="Hitesh"
+
+let age = 18
+let isLoggedin = false 
+
+let state; // undefined
+
+// number => 2 to power 53
+//bigint
+// string =>""
+// boolean =>true /false
+// null => standalone value
+
+// undefined => 
+
+    // symbol = unique 
+
+
+    // object 
+
+console.log(typeof undefined) // undefined
+
+console.log(typeof null);    // object
+
+
